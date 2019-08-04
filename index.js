3b29bacd820ee4ff0f5e5ebbab1ceb56485a7fe2
Saruggan Thiruchelvan <40643067+tsaruggan@users.noreply.github.com>
2019-08-04 19:05:52 -0400

    created port from environment variable

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 const API_KEY = process.env.API_KEY;
 
-app.listen(3000, () => console.log("listening at port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening at port ${port}`));
 app.use(express.static('client'));
 app.use(express.json());
 
